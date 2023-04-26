d3868d5035c517804debf175962d1f6ee42f9286
FRANCE-AD\T150880 <saberrada-ext@airfrance.fr>
2023-04-26 16:54:29 +0200

    new

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php echo '<p>Bonjour le monde</p>'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello, World!</title>
+</head>
+<body>
+        <h1><?php echo 'Hello, World!'; ?></h1>
+		<h2>
+		<?php
+			$name = $_SERVER['HTTP_X_MS_CLIENT_PRINCIPAL_NAME'];
+			echo "hello" $name;
+		?>
+		</h2>
+</body>
+</html>
